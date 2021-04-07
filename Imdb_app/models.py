770ab54abf56970dd8cdbaa110e50b3643abfb71
JoseGuevara01 <jose.urquiza.guevara@gmail.com>
2021-04-07 11:53:08 -0400

    user model x comment model

--- Imdb_app/models.py
+++ Imdb_app/models.py
@@ -1,38 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django_mysql.models import ListCharField
+from django.utils import timezone
 # Create your models here.
 
+# user model
 class ApplicationUser(AbstractUser):
     display_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-
+    likes = ListCharField(
+        base_field=models.URLField(max_length=500),
+        max_length=5000
+    )
+    want_to_see = ListCharField(
+        base_field=models.URLField(max_length=500), 
+        max_length=5000
+    )
+    have_seen = ListCharField(
+        base_field=models.URLField(max_length=500),
+        max_length=5000, 
+    )
+    # fav_genres = models.ManyToManyField(Genre)
+    # email = models.EmailField(max_length=50)
+    
     REQUIRED_FIELDS = ['display_name']
-    # Maybe have a favorite genre connected via FK or manytomany 
-
-# rename to Movie_Model
-class Movie(models.Model):
-    name = models.CharField(max_length=150)
-    date_released = models.DateField()
-    genre = models.CharField(max_length=50)
 
 
-    def __str__(self):
-        return f'{self.name}'
-    # actors -- Maybe connect actors via a manytomany 
-    # Language, Budget, 
-
-class MovieReview(models.Model):
-    review = models.TextField(max_length=250)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    user = models.ForeignKey(ApplicationUser, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return f'{self.user} {self.review} {self.movie}'
-    
-class Actor(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    # possibly add link to their social media profile
+class Comment_model(models.Model):
+    input_field = models.TextField()
+    commenter = models.ForeignKey(ApplicationUser, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
+    recommended = models.BooleanField(null=True)
 
 
+
+    # can go with being a simple foreignKey or having a list via manytomany
 # Genre Model connect with Movie and Music
