87f89d089b6a5574c0cca3368d5a07fd321382c7
Jordan Kubista <jkube29@gmail.com>
2021-04-08 16:17:38 -0500

    search header is working

--- Imdb_app/forms.py
+++ Imdb_app/forms.py
@@ -1,9 +1,9 @@
 
 from django import forms
-from Imdb_app.models import ApplicationUser
+from Imdb_app.models import ApplicationUser, Comment_model
 
 class SignupForm(forms.Form):
-    display_name = models.CharField(max_length=50)
+    display_name = forms.CharField(max_length=50)
     # Was unsure of to whether to include the likes,want_to_see, and have_seen in the inital signup or rather after
 
 
@@ -18,3 +18,6 @@ class Comment_Form(forms.ModelForm):
             'date_created',
             'recommended'
         ]
+
+class SearchForm(forms.Form):
+    search_selection = forms.CharField(max_length=180)
--- Imdb_app/views.py
+++ Imdb_app/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
-
+import requests
+from Imdb_app.forms import SearchForm
 
 # Create your views here.
+def home_page_view(request):
+    context = {}
+
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            print(data['search_selection'])
+            # Api Call With the Search Results
+            url = "https://imdb8.p.rapidapi.com/auto-complete"
+            querystring = {"q": data['search_selection']}
+
+            headers = {
+                'x-rapidapi-key': "1ddf0a8da3msh877010e622bf74dp10873cjsnd762a292965a",
+                'x-rapidapi-host': "imdb8.p.rapidapi.com"
+                }
+
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            
+            reply = response.json()
+            print(response.json())
+    form = SearchForm()
+    context.update({'form': form, 'reply': reply})
+    return render(request, 'homepage.html', context)
