6d80c359540a5cea9a00d72397aab6c8625b5b39
Jordan Kubista <jkube29@gmail.com>
2021-04-13 15:30:44 -0500

    updated the comment model and form

--- Imdb_app/admin.py
+++ Imdb_app/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import ApplicationUser, Comment_model, LikedMoviesModel, WantToSeeModel, HaveSeenModel
+from .models import ApplicationUser, Comment_model, LikedMoviesModel
+from .models import WantToSeeModel, HaveSeenModel
+
+
 # Register your models here.
 class CustomAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
-        ('Custom Fields', {'fields': ('display_name','likes','want_to_see','have_seen')}),
+        ('Custom Fields', {'fields': ('display_name',)}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Custom Fields', {'fields': ('display_name','likes','want_to_see','have_seen')}),
+        ('Custom Fields', {'fields': ('display_name',)}),
     )
-    
+
+
 admin.site.register(ApplicationUser, CustomAdmin)
 admin.site.register(Comment_model)
 admin.site.register(LikedMoviesModel)
